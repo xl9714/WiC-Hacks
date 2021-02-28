b4dcd7893071bf784070a5e366359cbb75b4414e
Xinyi Liu <xl9714@rit.edu>
2021-02-28 01:04:05 -0500

    commit 1

---
+++ src/data/editTable.java
@@ -0,0 +1,11 @@
+package data;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class editTable extends JFrame{
+
+}
